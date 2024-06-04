d46c96abcb4175eb23c6d65835d7fb1e469c0f74
Babette Baekgaard <bab@solita.dk>
2024-06-04 14:35:27 +0200

    added a deferance of the list of facts

--- learn-angular/src/app/app.component.ts
+++ learn-angular/src/app/app.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
+import { FactService } from './services/fact.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    UserComponent
+    UserComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   city = 'San Francisco';
   message = '';
   turtles = '';
@@ -20,7 +21,14 @@ export class AppComponent {
   isServerRunning = true;
   isEditable = true;
 
+  facts: { id: number, fact: string }[] = [];
   users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}];
+
+  constructor(private factService : FactService) {}
+
+  async ngOnInit() {
+    this.facts = await this.factService.getAllFacts();
+  }
 
   onMouseOver() {
     this.message = 'Way to go 🚀';
