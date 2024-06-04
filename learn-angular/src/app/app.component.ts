e587550b563575ed0270533de57d335b81882859
Babette Baekgaard <bab@solita.dk>
2024-06-04 12:21:08 +0200

    added iterable variable to display iteration in the ui

--- learn-angular/src/app/app.component.ts
+++ learn-angular/src/app/app.component.ts
@@ -16,4 +16,6 @@ export class AppComponent {
   city = 'San Francisco';
 
   isServerRunning = true;
+
+  users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}];
 }
